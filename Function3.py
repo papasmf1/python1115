918f0130802a6b78d9c305b22fff2b22c9d407d4
papasmf1 <papasmf1@gmail.com>
2021-11-16 10:05:05 +0900

    3장 함수 완료

--- Function3.py
+++ Function3.py
@@ -58,3 +58,10 @@ print( userURIBuilder("naver.com", "80", id="kim", passwd="1234") )
 print( userURIBuilder("naver.com", "80", id="kim", passwd="1234", 
     name="mike") )
 
+#람다함수(간결한 함수 표현식): 익명함수 
+g = lambda x,y:x*y
+print( g(3,4) )
+print( g(5,6) )
+print( (lambda x:x*x)(3) )
+print( globals() )
+
