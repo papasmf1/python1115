e32ea9ce9bc0021fc09ad7747d24e6cac40dafeb
papasmf1 <papasmf1@gmail.com>
2021-11-16 13:31:48 +0900

    클래스 연습 1

---
+++ class1.py
@@ -0,0 +1,16 @@
+# class1.py 
+#클래스를 정의
+class Person:
+    #생성자(초기화 메서드)
+    def __init__(self):
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+#인스턴스 생성
+p1 = Person()
+p1.name = "전우치"
+p2 = Person()
+p1.print()
+p2.print() 
+
