0980cca0ee2d825754205d49e8b9be61b50692b0
papasmf1 <papasmf1@gmail.com>
2021-11-16 09:38:42 +0900

    함수 연습 4

--- Function3.py
+++ Function3.py
@@ -24,5 +24,25 @@ print( times() )
 print( times(3) )
 print( times(3,4) )
 
+#키워드 인자 
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port 
+    return strURL 
 
+#호출
+print( connectURI("naver.com", "80") )
+print( connectURI(port="80", server="naver.com") )
+
+#가변인자(다양한 상황)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print( union("HAM","EGG") )
+print( union("HAM","EGG","SPAM") )
 
