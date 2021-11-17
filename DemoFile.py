1ad9c6d7bd5610b289d520158bc4df216799e7da
papasmf1 <papasmf1@gmail.com>
2021-11-17 11:24:46 +0900

    파일 연습 완료

--- DemoFile.py
+++ DemoFile.py
@@ -1,9 +1,21 @@
 # DemoFile.py 
 
+#한글로 쓰기와 읽기
 f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
 f.write("첫번째라인\n두번째\n세번째\n")
 f.close()
 
 print( f.closed )
 
-
+#읽기 
+f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
+print("---read()---")
+print( f.read() )
+print("---readline()---")
+print( f.tell() )
+f.seek(0)
+print( f.readline() )
+print( f.readline() )
+print("---readlines()---")
+f.seek(0)
+print( f.readlines() )
