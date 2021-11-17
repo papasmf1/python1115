d4187398051ed69744fc22b205eabaf8b79a506c
papasmf1 <papasmf1@gmail.com>
2021-11-17 15:25:40 +0900

    파일 폴더리스트 2

--- DemoOS.py
+++ DemoOS.py
@@ -16,8 +16,15 @@ print( getsize("c:\\python38\\python.exe") )
 #운영체제 정보 
 import os 
 print("운영체제이름:", os.name)
-os.system("notepad.exe")
+#os.system("notepad.exe")
 
 #파일 리스트, 폴더 리스트 
 import glob 
 print( glob.glob("c:\\work\\*.py") )
+
+os.chdir("..")
+print("현재 작업 폴더:", os.getcwd() )
+os.chdir("c:\\work")
+for item in glob.glob("*.*"):
+    print(item)
+
