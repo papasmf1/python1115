bd43a9320915a6868bcb0980d7560d4e43b554af
papasmf1 <papasmf1@gmail.com>
2021-11-18 10:52:44 +0900

    크롤링 연습2

--- web1.py
+++ web1.py
@@ -1,11 +1,22 @@
 # web1.py 
 from bs4 import BeautifulSoup
 
-#페이지를 로딩
+#페이지를 로딩: 연속으로 메서드 체인, 함수 체인 
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체 생성
 soup = BeautifulSoup(page, "html.parser")
 #전체를 보여달라~
-print( soup.prettify() )
+#print( soup.prettify() )
+
+#<p>를 몽땅 검색 ==> List형식으로 리턴 
+#print( soup.find_all("p") )
+
+#첫번째 <p> 검색
+#print( soup.find("p") )
+
+#필터링:<p class="outer-text">
+#파이썬에서 클래스를 정의하는 키워드 class:  class_
+print( soup.find_all("p", class_="outer-text") )
+
 
 
